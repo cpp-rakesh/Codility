ed62f2a39558b2f83037b4f67420e7809eed05bb
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-03-13 20:26:33 +1100

    solution for Cyclic Rotation

---
+++ lessons/lesson_2/cyclic_rotation.cpp
@@ -0,0 +1,41 @@
+/*
+  https://app.codility.com/programmers
+  Problem Cyclic Rotation.
+  Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: 13/03/2018
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int>& v, int k) {
+    std::vector<int> r(v.size(), 0);
+    for (size_t i = 0; i < v.size(); ++i)
+        r[(i + k) % v.size()] = v[i];
+
+    return r;
+}
+
+int random(int s, int e) {
+    return s + rand() % (e - s + 1);
+}
+
+void print(const vector<int>& v) {
+    printf("---------------------------------------------------\n");
+    for (size_t i = 0; i < v.size(); ++i)
+        printf("%d ", v[i]);
+    printf("\n---------------------------------------------------\n");
+}
+
+int main() {
+    vector<int> v;
+    for (int i = 0; i < 10; ++i)
+        v.push_back(random(1, 10));
+
+    print(v);
+    v = solution(v, 3);
+    print(v);
+
+    return 0;
+}
