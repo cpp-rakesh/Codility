99ecc32fb183c99d23ab03a5bcc8420f3074357e
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-03-15 19:36:09 +1100

    exercise 1

---
+++ exercises/chapter_2/example_1.cpp
@@ -0,0 +1,46 @@
+/*
+  https://app.codility.com/programmers
+  Chapter 1 Exercise 1.
+  Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: 15/03/2018
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int days(const vector<int>& v) {
+    int c = 0;
+    for (size_t i = 0; i < v.size(); ++i)
+        if (v[i] < 0)
+            ++c;
+
+    return c;
+}
+
+inline int random(int s, int e) {
+    return s + rand() % (e - s + 1);
+}
+
+void print(const vector<int>& v) {
+    printf("---------------------------------------\n");
+    for (size_t i = 0; i < v.size(); ++i)
+        printf("%d ", v[i]);
+    printf("\n---------------------------------------\n");
+}
+
+vector<int> get(int n) {
+    vector<int> v;
+    for (int i = 0; i < n; ++i)
+        v.push_back(random(-20, 40));
+
+    return v;
+}
+
+int main() {
+    const vector<int> v = get(30);
+    print(v);
+    printf("Cold days == [%d]\n", days(v));
+
+    return 0;
+}
