9aa5b103de6148cf759344189031ba8ebe583d40
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-03-16 12:28:07 +1100

    solution for example 1

---
+++ exercises/chapter_6/example_1.cpp
@@ -0,0 +1,49 @@
+/*
+  https://app.codility.com/programmers
+  Chapter 6 Exercise 1.
+  Solution by Rakesh Kumar, cpp.rakesh(at)gmail.com
+  Date: 16/03/2018
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline int random(int s, int e) {
+    return s + rand() % (e - s + 1);
+}
+
+inline vector<int> get(int n) {
+    vector<int> r;
+    for (int i = 0; i < n; ++i)
+        r.push_back(random(1, 20));
+
+    return r;
+}
+
+inline void print(const vector<int>& v) {
+    printf("----------------------------------------------\n");
+    for (size_t i = 0; i < v.size(); ++i)
+        printf("%d ", v[i]);
+    printf("\n----------------------------------------------\n");
+}
+
+int distinct(vector<int>& v) {
+    sort(v.begin(), v.end());
+
+    int c = 0;
+    for (size_t i = 1; i < v.size(); ++i)
+        if (v[i] != v[i - 1])
+            ++c;
+
+    return c;
+}
+
+
+int main() {
+    vector<int> v = get(20);
+    print(v);
+    printf("Distinct elements == [%d]\n", distinct(v));
+
+    return 0;
+}
