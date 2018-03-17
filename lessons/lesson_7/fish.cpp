064b1b1f9e38e21546cafedf353c10f72432eef7
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-03-18 01:23:07 +1100

    solution for Fish

--- lessons/lesson_7/fish.cpp
+++ lessons/lesson_7/fish.cpp
@@ -10,23 +10,31 @@
 using namespace std;
 
 int solution(vector<int>& a, vector<int>& b) {
-    int live = a.size();
-    stack<int> = battle;
+    int alive = 0;
+    stack<int> battle;
 
     for (size_t i = 0; i < a.size(); ++i) {
-        if (b[i] == 1) {
-            battle.push(a[i]);
+        if (b[i] == 0) {
+            if (battle.empty()) {
+                ++alive;
+            } else {
+                while (battle.size() && a[i] > battle.top())
+                    battle.pop();
+
+                if (battle.empty())
+                    ++alive;
+            }
         } else {
-            
+            battle.push(a[i]);
         }
     }
 
-    return battle.size();
+    return alive + battle.size();
 }
 
 int main() {
     vector<int> a = {4, 3, 2, 1, 5};
-    vector<int> b = {1, 1, 0, 1, 0};
+    vector<int> b = {0, 1, 0, 0, 0};
     printf("%d\n", solution(a, b));
 
     return 0;
