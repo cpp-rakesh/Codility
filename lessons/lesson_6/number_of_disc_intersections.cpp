9d8b2b132d2d52a5e81bd48fb8b4d0d0e6de7412
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-03-18 02:06:28 +1100

    solution for Number of Disc Intersections

--- lessons/lesson_6/number_of_disc_intersections.cpp
+++ lessons/lesson_6/number_of_disc_intersections.cpp
@@ -9,7 +9,35 @@
 
 using namespace std;
 
+const int limit = 1e+7;
+
+int solution(vector<int>& a) {
+    int c = 0;
+    vector<int> intersect(a.size() + 1, 0);
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (static_cast<int>(i) <= a[i]) {
+            c += i;
+        }
+        else {
+            c += a[i];
+            c += i - a[i] - intersect[i - a[i]];
+        }
+
+        if (c > limit)
+            return -1;
+
+        intersect[i + 1] += intersect[i];
+        if (a[i] < static_cast<int>(a.size()) && i + a[i] + 1 < a.size())
+            ++intersect[i + a[i] + 1];
+    }
+
+    return c;
+}
+
 int main() {
+    vector<int> v = {1, 5, 2, 1, 4, 0};
+    printf("%d\n", solution(v));
 
     return 0;
 }
