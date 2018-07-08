3d5fc59d77bb9f9a0f081516b7bb60268648e490
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-07-08 20:52:14 +1000

    random generator optimization

--- exercises/chapter_2/example_2.cpp
+++ exercises/chapter_2/example_2.cpp
@@ -6,6 +6,7 @@
 */
 
 #include <bits/stdc++.h>
+#include <random>
 
 using namespace std;
 
@@ -24,7 +25,9 @@ void reverse(vector<int>& v) {
 }
 
 inline int random(int s, int e) {
-    return s + rand() % (e - s + 1);
+    random_device rd;
+    uniform_int_distribution<> dt(s, e);
+    return dt(rd);
 }
 
 void print(const vector<int>& v) {
